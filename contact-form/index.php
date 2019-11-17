bc483c8cb5a0d625c8dff9d26ee981872dd054db
Pengyuan Li <lpy199454@gmail.com>
2019-11-17 02:34:04 -0500

    update

--- contact-form/index.php
+++ contact-form/index.php
@@ -1,4 +1,46 @@
-<!doctype html>  
+<?php
+	// Start session.
+	session_start();
+	
+	// Set a key, checked in mailer, prevents against spammers trying to hijack the mailer.
+	$security_token = $_SESSION['security_token'] = uniqid(rand());
+	
+	if ( ! isset($_SESSION['formMessage'])) {
+		$_SESSION['formMessage'] = 'Fill in the form below to send me an email.';	
+	}
+	
+	if ( ! isset($_SESSION['formFooter'])) {
+		$_SESSION['formFooter'] = ' ';
+	}
+	
+	if ( ! isset($_SESSION['form'])) {
+		$_SESSION['form'] = array();
+	}
+	
+	function check($field, $type = '', $value = '') {
+		$string = "";
+		if (isset($_SESSION['form'][$field])) {
+			switch($type) {
+				case 'checkbox':
+					$string = 'checked="checked"';
+					break;
+				case 'radio':
+					if($_SESSION['form'][$field] === $value) {
+						$string = 'checked="checked"';
+					}
+					break;
+				case 'select':
+					if($_SESSION['form'][$field] === $value) {
+						$string = 'selected="selected"';
+					}
+					break;
+				default:
+					$string = $_SESSION['form'][$field];
+			}
+		}
+		return $string;
+	}
+?><!doctype html>  
 
 <!--[if IE 7 ]><html lang="en" class="no-js ie7"><![endif]-->
 <!--[if IE 8 ]><html lang="en" class="no-js ie8"><![endif]-->
@@ -19,14 +61,13 @@
   gtag('config', 'UA-132321370-1');
 </script><link rel="mask-icon" href="../logo4.svg" color="#000000" />
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="description" content="Patrick Li, Analytics Consultant, Machine Learning Data Engineer at CVS Health." />
 		<meta name="robots" content="index, follow" />
 		<meta name="generator" content="RapidWeaver" />
 		<link rel="icon" href="http://lipengyuan1994.github.io/favicon.ico" type="image/x-icon" />
 		<link rel="shortcut icon" href="http://lipengyuan1994.github.io/favicon.ico" type="image/x-icon" />
 		<link rel="apple-touch-icon" href="http://lipengyuan1994.github.io/apple-touch-icon.png" />
 		
-		<title>About Patrick Li</title>
+		<title>Contact Me | Patrick Li 🌰</title>
 		<link rel="stylesheet" type="text/css" media="all" href="../rw_common/themes/allegro/consolidated.css?rwcache=571976335" />
 		
 		
@@ -63,12 +104,12 @@
 				<div class="container">
 		   			<a id="logo" href="https://lipengyuan1994.github.io/" title="Home"></a>
 					<h1 id="title">Patrick Li 🌰</h1>
-					<nav><ul><li><a href="../" rel="">HOME</a></li><li><a href="../blog-4/" rel="">BLOG</a></li><li><a href="../blog/" rel="">NEWS</a></li><li><a href="./" rel="" id="current">About Me</a></li><li><a href="../page-3/" rel="" class="parent">More About Me</a><ul><li><a href="../page-3/page/" rel="">CERTIFICATES</a></li><li><a href="../page-3/page-2/" rel="">COURSES</a></li><li><a href="../page-3/blog-5/" rel="">PROJECTS</a></li><li><a href="../page-3/blog-3/" rel="">COMPETITIONS</a></li></ul></li><li><a href="../contact-form/" rel="">Contact Me</a></li></ul></nav>
+					<nav><ul><li><a href="../" rel="">HOME</a></li><li><a href="../blog-4/" rel="">BLOG</a></li><li><a href="../blog/" rel="">NEWS</a></li><li><a href="../markdown/" rel="">About Me</a></li><li><a href="../page-3/" rel="" class="parent">More About Me</a><ul><li><a href="../page-3/page/" rel="">CERTIFICATES</a></li><li><a href="../page-3/page-2/" rel="">COURSES</a></li><li><a href="../page-3/blog-5/" rel="">PROJECTS</a></li><li><a href="../page-3/blog-3/" rel="">COMPETITIONS</a></li></ul></li><li><a href="./" rel="" id="current">Contact Me</a></li></ul></nav>
 				</div><!-- end container -->
 			</div><!--end headerWrapper-->	
 			<nav id="navi-wrap">
 				<ul id="navi">
-					<ul><li><a href="../" rel="">HOME</a></li><li><a href="../blog-4/" rel="">BLOG</a></li><li><a href="../blog/" rel="">NEWS</a></li><li><a href="./" rel="" id="current">About Me</a></li><li><a href="../page-3/" rel="" class="parent">More About Me</a><ul><li><a href="../page-3/page/" rel="">CERTIFICATES</a></li><li><a href="../page-3/page-2/" rel="">COURSES</a></li><li><a href="../page-3/blog-5/" rel="">PROJECTS</a></li><li><a href="../page-3/blog-3/" rel="">COMPETITIONS</a></li></ul></li><li><a href="../contact-form/" rel="">Contact Me</a></li></ul>
+					<ul><li><a href="../" rel="">HOME</a></li><li><a href="../blog-4/" rel="">BLOG</a></li><li><a href="../blog/" rel="">NEWS</a></li><li><a href="../markdown/" rel="">About Me</a></li><li><a href="../page-3/" rel="" class="parent">More About Me</a><ul><li><a href="../page-3/page/" rel="">CERTIFICATES</a></li><li><a href="../page-3/page-2/" rel="">COURSES</a></li><li><a href="../page-3/blog-5/" rel="">PROJECTS</a></li><li><a href="../page-3/blog-3/" rel="">COMPETITIONS</a></li></ul></li><li><a href="./" rel="" id="current">Contact Me</a></li></ul>
 				</ul>
 			</nav><!-- /nav-wrap -->
 		<div id="banner">
@@ -78,7 +119,7 @@
 		<div id="bttmHeader">	
 		<div class="container">
 			<div class="sixteen columns over">
-				<h2 id="slogan">I am a slow walker , but I never walk backwards.(Abraham Lincoln)</h2>
+				<h2 id="slogan">There is nowhere to go, but up. </h2>
 			</div><!-- header-inner -->
 		</div><!-- end container -->	
 		</div><!-- end bttmHeader -->
@@ -91,7 +132,38 @@
 		</div><!-- end wrapper -->
 			<div class="container">
 				<div class="single twelve columns"  id="main" role="main">
-					<h2 id="willupdate">Will update</h2>
+					
+<div class="message-text"><?php echo $_SESSION['formMessage']; unset($_SESSION['formMessage']); ?></div><br />
+
+<form class="rw-contact-form" action="./files/mailer.php" method="post" enctype="multipart/form-data">
+	 <div>
+		<label>Your Name</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element0'); ?>" name="form[element0]" size="40"/><br /><br />
+
+		<label>Your Email</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element1'); ?>" name="form[element1]" size="40"/><br /><br />
+
+		<label>Subject</label> *<br />
+		<input class="form-input-field" type="text" value="<?php echo check('element2'); ?>" name="form[element2]" size="40"/><br /><br />
+
+		<label>Message</label> *<br />
+		<textarea class="form-input-field" name="form[element3]" rows="8" cols="38"><?php echo check('element3'); ?></textarea><br /><br />
+
+		<div style="display: none;">
+			<label>Spam Protection: Please don't fill this in:</label>
+			<textarea name="comment" rows="1" cols="1"></textarea>
+		</div>
+		<input type="hidden" name="form_token" value="<?php echo $security_token; ?>" />
+		<input class="form-input-button" type="reset" name="resetButton" value="Reset???" />
+		<input class="form-input-button" type="submit" name="submitButton" value="Submit!!!" />
+	</div>
+</form>
+
+<br />
+<div class="form-footer"><?php echo $_SESSION['formFooter']; unset($_SESSION['formFooter']); ?></div><br />
+
+<?php unset($_SESSION['form']); ?>
+
 				</div><!-- end single twelve column -->		
 				<div class="sidebar four columns offset-by-one">
 					<aside>
